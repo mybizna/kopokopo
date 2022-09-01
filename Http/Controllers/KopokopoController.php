98bdf6387fbc3b48a9195f3d7c8e41fd8b4fa447
Dedan Irungu <dedanirungu@gmail.com>
2022-09-01 15:14:52 +0300

    update to commands

---
+++ Http/Controllers/KopokopoController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Modules\Mpesa\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Modules\Base\Http\Controllers\BaseController;
+
+class KopokopoController extends BaseController
+{
+
+    public function simulate(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+
+    public function stkpush(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function buygoods_transaction_received(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function buygoods_transaction_reversed(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function b2b_transaction_received(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function m2m_transaction_received(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function settlement_transfer_completed(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+    public function customer_created(Request $request)
+    {
+        $result = [];
+        return response()->json($result);
+    }
+}
