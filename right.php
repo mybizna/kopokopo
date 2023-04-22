72a9857825e76b962a294ec93162dd3424304bae
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,141 @@
+<?php
+
+$this->add_right("kopokopo", "gateway", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "gateway", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "gateway", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "gateway", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "gateway", "registered", view:true, add:true);
+$this->add_right("kopokopo", "gateway", "guest", view:true, );
+
+$this->add_right("kopokopo", "pay", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "pay", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "pay", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "pay", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "pay", "registered", view:true, add:true);
+$this->add_right("kopokopo", "pay", "guest", view:true, );
+
+$this->add_right("kopokopo", "payment", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "payment", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "payment", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "payment", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "payment", "registered", view:true, add:true);
+$this->add_right("kopokopo", "payment", "guest", view:true, );
+
+$this->add_right("kopokopo", "recipient", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "recipient", "registered", view:true, add:true);
+$this->add_right("kopokopo", "recipient", "guest", view:true, );
+
+$this->add_right("kopokopo", "recipient_bank", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_bank", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_bank", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_bank", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "recipient_bank", "registered", view:true, add:true);
+$this->add_right("kopokopo", "recipient_bank", "guest", view:true, );
+
+$this->add_right("kopokopo", "recipient_mobile", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_mobile", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_mobile", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_mobile", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "recipient_mobile", "registered", view:true, add:true);
+$this->add_right("kopokopo", "recipient_mobile", "guest", view:true, );
+
+$this->add_right("kopokopo", "recipient_paybill", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_paybill", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_paybill", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_paybill", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "recipient_paybill", "registered", view:true, add:true);
+$this->add_right("kopokopo", "recipient_paybill", "guest", view:true, );
+
+$this->add_right("kopokopo", "recipient_till", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_till", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_till", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "recipient_till", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "recipient_till", "registered", view:true, add:true);
+$this->add_right("kopokopo", "recipient_till", "guest", view:true, );
+
+$this->add_right("kopokopo", "stkpush", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "stkpush", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "stkpush", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "stkpush", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "stkpush", "registered", view:true, add:true);
+$this->add_right("kopokopo", "stkpush", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_b2b", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_b2b", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_b2b", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_b2b", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_b2b", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_b2b", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_buygood", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_buygood", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_buygood", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_buygood", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_buygood", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_buygood", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_customer", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_customer", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_customer", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_customer", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_customer", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_customer", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_merchant", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_merchant", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_merchant", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_merchant", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_merchant", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_merchant", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_reversal", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_reversal", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_reversal", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_reversal", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_reversal", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_reversal", "guest", view:true, );
+
+$this->add_right("kopokopo", "transaction_transfer", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_transfer", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_transfer", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "transaction_transfer", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "transaction_transfer", "registered", view:true, add:true);
+$this->add_right("kopokopo", "transaction_transfer", "guest", view:true, );
+
+$this->add_right("kopokopo", "webhook", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "webhook", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "webhook", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "webhook", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "webhook", "registered", view:true, add:true);
+$this->add_right("kopokopo", "webhook", "guest", view:true, );
+
+$this->add_right("kopokopo", "withdraw", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "withdraw", "registered", view:true, add:true);
+$this->add_right("kopokopo", "withdraw", "guest", view:true, );
+
+$this->add_right("kopokopo", "withdraw_bank", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_bank", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_bank", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_bank", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "withdraw_bank", "registered", view:true, add:true);
+$this->add_right("kopokopo", "withdraw_bank", "guest", view:true, );
+
+$this->add_right("kopokopo", "withdraw_mobile", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_mobile", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_mobile", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("kopokopo", "withdraw_mobile", "staff", view:true, add:true, edit:true);
+$this->add_right("kopokopo", "withdraw_mobile", "registered", view:true, add:true);
+$this->add_right("kopokopo", "withdraw_mobile", "guest", view:true, );
