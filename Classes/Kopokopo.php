0927337a0bba9e3253095caa03c303377f1ad995
Dedan <dedanirungu@gmail.com>
2023-09-06 22:40:44 +0300

    Release 23.09.001

---
+++ Classes/Kopokopo.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Modules\Kopokopo\Classes;
+
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Modules\Account\Classes\PaymentProcessor;
+use Modules\Account\Entities\Transaction;
+use Modules\Kopokopo\Entities\Payment as Kopokopopayment;
+use Modules\Kopokopo\Entities\Transaction as Kopokopotransaction;
+
+class Kopokopo
+{
+    public function paymentKopokopoCancel(Request $request)
+    {
+        $paymentProcessor = new PaymentProcessor();
+        $paymentId = $request->input('payment_id');
+        $payment = $paymentProcessor->getPaymentById($paymentId);
+        $paymentProcessor->cancelTransaction($payment);
+        return $payment;
+    }
+
+    public function paymentKopokopoReturn(Request $request)
+    {
+        $paymentProcessor = new PaymentProcessor();
+        $paymentId = $request->input('payment_id');
+        $payment = $paymentProcessor->getPaymentById($paymentId);
+        $paymentProcessor->completeTransaction($payment);
+        return $payment;
+    }
+
+    public function paymentKopokopoNotify(Request $request)
+    {
+        $paymentProcessor = new PaymentProcessor();
+        $paymentId = $request->input('payment_id');
+        $mpesaCode = $request->input('mpesa_code');
+        $payment = $paymentProcessor->getPaymentById($paymentId);
+        $processToken = $this->processKopokopo($payment, $mpesaCode);
+
+        if ($processToken) {
+            $paymentProcessor->successfulTransaction($payment, $mpesaCode);
+        } else {
+            $paymentProcessor->failTransaction($payment);
+        }
+
+        return $payment;
+    }
+
+    public function processKopokopo($payment, $mpesaCode)
+    {
+        $paymentProcessor = new PaymentProcessor();
+        $isValid = true;
+        $message = '';
+
+        $gateway = $paymentProcessor->getGatewayByName('kopokopo');
+
+        $kopokopoObj = $this->getKopokopoTransaction($mpesaCode);
+
+        if (!$kopokopoObj) {
+            $message = $message . 'Mpesa Code (' . $mpesaCode . ') does not exist or is already used.';
+            return false;
+        }
+
+        $deductions = [];
+
+        if (!empty($payment->deductions) && strpos($payment->deductions, '{') !== false) {
+            $deductions = json_decode($payment->deductions, true);
+        }
+
+        $requiredAmount = isset($deductions['amount']) ? $deductions['amount'] : $payment->amount;
+        $paidAmount = $kopokopoAmount = $kopokopoObj->amount ?: 0;
+        $paidAmount = $paymentProcessor->getGatewayConverterAmount($paidAmount, $gateway, false);
+
+        $payment->code = $mpesaCode;
+        $payment->gateway = $gateway;
+        $payment->save();
+
+        $paymentProcessor->savePaidAmount($payment, $requiredAmount, $paidAmount);
+
+        if ($paidAmount <= $requiredAmount) {
+            $isValid = false;
+        } else {
+            if ($payment->source->model_name !== 'Wallet') {
+                Transaction::create([
+                    'user' => $payment->user,
+                    'from_user' => $payment->user,
+                    'payment' => $payment,
+                    'description' => $payment->description . ' [Txn:' . $payment->id . ']',
+                    'money_out' => $payment->amount,
+                    'year' => (new DateTime())->format('Y'),
+                    'month' => (new DateTime())->format('m'),
+                    'type' => 'token',
+                    'source' => $payment->source,
+                    'source_ident' => $payment->source_ident,
+                ]);
+            }
+        }
+
+        $this->saveKopokopoPayment($payment, $paidAmount, $mpesaCode, $kopokopoObj->sender_phone);
+        $this->updateKopokopo($kopokopoObj, $payment->user);
+
+        return $isValid;
+    }
+
+    public function saveKopokopoPayment($payment, $amount, $mpesaCode, $phone)
+    {
+        Kopokopopayment::create([
+            'transaction_reference' => $mpesaCode,
+            'payment' => $payment,
+            'sender_phone' => $phone,
+            'amount' => $amount,
+        ]);
+    }
+
+    public function updateKopokopo($kopokopoObj, $user)
+    {
+        $kopokopoObj->used = true;
+        $kopokopoObj->date_used = now();
+        $kopokopoObj->user = $user;
+        $kopokopoObj->save();
+    }
+
+    public function getKopokopoTransaction($mpesaCode)
+    {
+        if ($mpesaCode) {
+            return Kopokopotransaction::where([
+                ['used', false],
+                ['transaction_reference', '=', trim($mpesaCode)],
+            ])->first();
+        } else {
+            return false;
+        }
+    }
+
+    public function syncKopokopoTransactions(Request $request)
+    {
+        $body = $request->getContent();
+        if (!$body) {
+            return false;
+        }
+
+        $content = json_decode($body, true);
+
+        $counter = Kopokopotransaction::where('transaction_reference', $content['transaction_reference'])->count();
+
+        if (!$counter) {
+            Kopokopotransaction::create([
+                'name' => $content['first_name'] . ' ' . $content['middle_name'] . ' ' . $content['last_name'],
+                'first_name' => $content['first_name'],
+                'middle_name' => $content['middle_name'],
+                'last_name' => $content['last_name'],
+                'service_name' => $content['service_name'],
+                'business_number' => $content['business_number'],
+                'transaction_reference' => $content['transaction_reference'],
+                'internal_transaction_id' => $content['internal_transaction_id'],
+                'transaction_timestamp' => $content['transaction_timestamp'],
+                'transaction_type' => $content['transaction_type'],
+                'account_number' => $content['account_number'],
+                'sender_phone' => $content['sender_phone'],
+                'amount' => $content['amount'],
+                'currency' => $content['currency'],
+            ]);
+        }
+
+        return true;
+    }
+}
--- Http/Controllers/KopokopoController.php
+++ Http/Controllers/KopokopoController.php
@@ -9,6 +9,8 @@ use Modules\Base\Http\Controllers\BaseController;
 class KopokopoController extends BaseController
 {
 
+    
+
     public function simulate(Request $request)
     {
         $result = [];
@@ -55,5 +57,151 @@ class KopokopoController extends BaseController
     {
         $result = [];
         return response()->json($result);
+    }
+
+    public function index(Request $request)
+    {
+        $context = [
+            'title' => "Kopokopopayment",
+        ];
+
+        return view('index', $context);
+    }
+
+    public function reset(Request $request, $pk)
+    {
+        $transaction = Kopokopotransaction::find($pk);
+
+        $transaction->user_id = null;
+        $transaction->used = false;
+        $transaction->save();
+
+        return redirect()->route('manage_paymentkopokopo_transaction_update', ['pk' => $transaction->id]);
+    }
+
+    public function verify(Request $request)
+    {
+        $enteredMpesaCode = $request->query('entered_mpesa_code');
+        $kopokopo = Kopokopotransaction::where('transaction_reference', $enteredMpesaCode)->first();
+        $gateway = Gateway::where('short_name', 'kopokopo')->first();
+        $converter = $gateway->currency ? $gateway->currency->rate : 1;
+        $status = false;
+        $payment = [];
+        $message = 'No Payment received with MPesa Code: ' . $enteredMpesaCode . '.<br> Please Click "Verify Payment" after a few minutes<br>If the issue persists, contact admin.';
+
+        if ($kopokopo && ($kopokopo->used || $kopokopo->user_id !== null)) {
+            $message = 'MPesa Code: ' . $enteredMpesaCode . ' is already used. Please contact admin for assistance.';
+        } elseif ($kopokopo && !$kopokopo->used) {
+            $status = true;
+            $payment = [
+                'transaction_reference' => $kopokopo->transaction_reference,
+                'amount' => $kopokopo->amount,
+                'converter_amount' => $kopokopo->amount / $converter,
+                'name' => $kopokopo->name,
+            ];
+            $message = 'Payment Successful';
+        }
+
+        $result = [
+            'message' => $message,
+            'status' => $status,
+            'payment' => $payment,
+            'list' => [],
+        ];
+
+        return JsonResponse::create($result);
+    }
+
+    public function paymentKopokopoReturn(Request $request)
+    {
+        $kopokopo = new Kopokopo();
+        $dbManager = new DBManager();
+
+        $payment = $kopokopo->paymentKopokopoReturn($request);
+
+        $response = $request->input('response');
+        if ($response === 'json') {
+            $message = 'Payment was Successful';
+            $newPayment = $dbManager->serialModel($payment);
+            $response = [
+                'status' => 200,
+                'message' => $message,
+                'payment' => $newPayment,
+            ];
+            return JsonResponse::create($response);
+        }
+
+        if ($payment->next_to) {
+            return redirect($payment->next_to);
+        } else {
+            return redirect('user_payment_list');
+        }
+    }
+
+    public function paymentKopokopoCancel(Request $request)
+    {
+        $kopokopo = new Kopokopo();
+        $dbManager = new DBManager();
+
+        $payment = $kopokopo->paymentKopokopoCancel($request);
+
+        $response = $request->input('response');
+        if ($response === 'json') {
+            $message = 'Payment was Cancelled';
+            $newPayment = $dbManager->serialModel($payment);
+            $response = [
+                'status' => 200,
+                'message' => $message,
+                'payment' => $newPayment,
+            ];
+            return JsonResponse::create($response);
+        }
+
+        if ($payment->next_to) {
+            return redirect($payment->next_to);
+        } else {
+            return redirect('user_payment_list');
+        }
+    }
+
+    public function paymentKopokopoNotify(Request $request)
+    {
+        $kopokopo = new Kopokopo();
+        $dbManager = new DBManager();
+
+        $payment = $kopokopo->paymentKopokopoNotify($request);
+
+        $response = $request->input('response');
+        if ($response === 'json') {
+            $message = 'Payment was Successful';
+            $newPayment = $dbManager->serialModel($payment);
+            $response = [
+                'status' => 200,
+                'message' => $message,
+                'payment' => $newPayment,
+            ];
+            return JsonResponse::create($response);
+        }
+
+        if ($payment->next_to) {
+            return redirect($payment->next_to);
+        } else {
+            return redirect('user_payment_list');
+        }
+    }
+
+    public function listener(Request $request)
+    {
+        $kopokopo = new Kopokopo();
+
+        $kopokopo->syncKopokopoTransactions($request);
+
+        $data = [
+            "status" => "01",
+            "description" => "Accepted",
+            "subscriber_message" => 'MPESA Payment received Successfully.',
+        ];
+
+        return JsonResponse::create($data);
     }
 }
